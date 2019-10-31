dc0e7d0e2814f7e86605e95f9743a5735ab7515e
maohangbin <271221198@qq.com>
2019-10-31 11:35:16 +0800

    路径优化

--- src/main/resources/static/js/path.js
+++ src/main/resources/static/js/path.js
@@ -5,4 +5,4 @@ var pathName = document.location.pathname;
 console.log(pathName)
 var pathLength = pathName.substr(1).indexOf(nextPath) + 1;
 console.log(pathLength)
-var pathHead = pathName.substr(0, pathLength);
+var pathHead = pathName.substr(0, pathLength) + nextPath;
